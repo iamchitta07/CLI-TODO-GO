9fdde05f44d57fdbd90c90510427a3a8e8e20541
iamchitta07 <nathchittajit8@gmail.com>
2026-05-26 22:29:21 +0530

    Initial Commit

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Inside main in todo app")
+	todos := Todos {}
+	todos.add("Buy milk")
+	todos.add("Buy bread")
+	todos.toggle(0)
+	todos.print()
+}
